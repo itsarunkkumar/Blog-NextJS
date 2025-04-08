c262cd2c394ed2a1a3b3281f52c30853fe0d2d8d
itsarunkkumar <arunyadav79499@gmail.com>
2025-04-08 23:33:26 +0530

    API integrated

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,57 +4,41 @@ import Styles from "./page.module.css"
 import Link from "next/link";
 import Image from "next/image";
 
+async function getData(){
+    const res = await fetch('https://api.vercel.app/blog',{
+        cache: "no-store"
+    });
+    if(!res.ok){
+        throw new Error('Failed to fetch Data');
+    }
 
-const Blog = () => {
+    return res.json();
+}
+
+
+const Blog =  async () => {
+    const data = await getData();
     return(
         <div className={Styles.maincontainer}>
-            <Link href="blog/test" className={Styles.container}>
-                <div className={Styles.imageContainer}>
-                    <Image 
-                        alt="blog image"
-                        width={400}
-                        height={250}
-                        className={Styles.image}
-                        src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
-                    />
-                </div>
-                <div className={Styles.content}>
-                    <h1 className={Styles.title}>Title</h1>
-                    <p className={Styles.desc}>Description</p>
-                </div>
-            </Link>
-
-            <Link href="blog/test" className={Styles.container}>
-                <div className={Styles.imageContainer}>
-                    <Image 
-                        alt="blog image"
-                        width={400}
-                        height={250}
-                        className={Styles.image}
-                        src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
-                    />
-                </div>
-                <div className={Styles.content}>
-                    <h1 className={Styles.title}>Title</h1>
-                    <p className={Styles.desc}>Description</p>
-                </div>
-            </Link>
-
-            <Link href="blog/test" className={Styles.container}>
-                <div className={Styles.imageContainer}>
-                    <Image 
-                        alt="blog image"
-                        width={400}
-                        height={250}
-                        className={Styles.image}
-                        src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
-                    />
-                </div>
-                <div className={Styles.content}>
-                    <h1 className={Styles.title}>Title</h1>
-                    <p className={Styles.desc}>Description</p>
-                </div>
-            </Link>
+            {data.map(item => (
+                <Link href={`blog/${item.id}`} className={Styles.container} key={item.id}>
+                    <div className={Styles.imageContainer}>
+                        <Image 
+                            alt="blog image"
+                            width={400}
+                            height={250}
+                            className={Styles.image}
+                            src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+                        />
+                    </div>
+                    <div className={Styles.content}>
+                        <h1 className={Styles.title}>{item.title}</h1>
+                        <p className={Styles.desc}>{item.content}</p>
+                        <p className={Styles.desc}>{item.category}</p>
+                        <p className={Styles.desc}>{item.author}</p>
+                    </div>
+                </Link> 
+            ))};
         </div>
     )
 }
